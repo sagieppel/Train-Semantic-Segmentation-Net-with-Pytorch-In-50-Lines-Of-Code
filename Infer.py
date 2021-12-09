580f3e93ab366da9bdf1c590e6c78b202e508d2a
sagieppel <sagieppel@gmail.com>
2021-12-09 04:56:15 -0500

    Update Infer.py

--- Infer.py
+++ Infer.py
@@ -3,7 +3,7 @@ import torchvision.models.segmentation
 import torch
 import torchvision.transforms as tf
 import matplotlib.pyplot as plt
-modelPath = "16000.torch"  # Path to trained model
+modelPath = "3000.torch"  # Path to trained model
 imagePath = "test.jpg"  # Test image
 height=width=900
 transformImg = tf.Compose([tf.ToPILImage(), tf.Resize((height, width)), tf.ToTensor(),tf.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))])  # tf.Resize((300,600)),tf.RandomRotation(145)])#
@@ -15,13 +15,14 @@ Net = Net.to(device)  # Set net to GPU or CPU
 Net.load_state_dict(torch.load(modelPath)) # Load trained model
 Net.eval() # Set to evaluation mode
 Img = cv2.imread(imagePath) # load test image
-plt.imshow(Img)  # Show image
+height_orgin , widh_orgin ,d = Img.shape # Get image original size 
+plt.imshow(Img[:,:,::-1])  # Show image
 plt.show()
 Img = transformImg(Img)  # Transform to pytorch
 Img = torch.autograd.Variable(Img, requires_grad=False).to(device).unsqueeze(0)
 with torch.no_grad():
     Prd = Net(Img)['out']  # Run net
-
-seg = torch.argmax(Prd[0], 0).cpu().detach().numpy()  # Get  prediction classes
+Prd = tf.Resize((height_orgin,widh_orgin))(Prd[0]) # Resize to origninal size
+seg = torch.argmax(Prd, 0).cpu().detach().numpy()  # Get  prediction classes
 plt.imshow(seg)  # display image
 plt.show()
